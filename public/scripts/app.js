257da84aa8969b12f44030527de1a97eceab8f88
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-02-06 21:50:10 +0000

    Change products api using request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -45,7 +45,6 @@ yelpREST('businesses/search', {
 })
 
 // MOVIES SEARCH
-const request = require ('request');
 let url = "https://api.themoviedb.org/3/search/movie?api_key=ae721a1684f55599ab48cd0e1a2b32db&query=avengers";
 
 request(url, (res, req) => {
@@ -54,25 +53,19 @@ request(url, (res, req) => {
 })
 
 // AMAZON PRODUCT SEARCH
-var unirest = require("unirest");
-var req = unirest("GET", "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search");
+const options = {
+  method: 'GET',
+  url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
+  qs: {keyword: 'iphone', country: 'US', category: 'aps'},
+  headers: {
+    'x-rapidapi-key': 'fabbb13a2amsh1109ab44850de4cp1b226ejsnc0a65e8d2e8d',
+    'x-rapidapi-host': 'amazon-product-reviews-keywords.p.rapidapi.com',
+    useQueryString: true
+  }
+};
 
-req.query({
-	"keyword": "iphone",
-	"country": "US",
-	"category": "aps"
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
 });
-
-req.headers({
-	"x-rapidapi-key": "fabbb13a2amsh1109ab44850de4cp1b226ejsnc0a65e8d2e8d",
-	"x-rapidapi-host": "amazon-product-reviews-keywords.p.rapidapi.com",
-	"useQueryString": true
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body.products);
-});
-
