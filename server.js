e62bd1927c3754717392a2d21895726510212571
unko-chan <kevinliang0303@gmail.com>
2021-02-06 12:24:34 -0800

    fix syntax error

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const todosRoutes = require("./routes/widgets");
+const todosRoutes = require("./routes/todos");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/todo", todosRoutes(db));
+app.use("/api/todos", todosRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
