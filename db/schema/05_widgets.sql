eaa4f3e3255e50b13cd7d1c5f37a1cc59e2d6776
Jacob Hein <jacobhein1892@gmail.com>
2021-02-06 04:08:06 +0000

    added basic structure for db's

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,12 @@
 -- Drop and recreate Widgets table (Example)
 
 DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
+CREATE TABLE todo (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  category VARCHAR(255) NOT NULL,
+  text VARCHAR(255) NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name) VALUES ('Beastie Boys');
+INSERT INTO users (name) VALUES ('WuTang');
+INSERT INTO users (name) VALUES ('Eazy-E');
+INSERT INTO users (name) VALUES ('Dr.Dre');
+INSERT INTO users (name) VALUES ('Snoooooooop');
+INSERT INTO users (name) VALUES ('Jay-Z');
+INSERT INTO users (name) VALUES ('Big L');
+INSERT INTO users (name) VALUES ('Tupac');
+INSERT INTO users (name) VALUES ('Biggy');
+INSERT INTO users (name) VALUES ('Ice Cube');
