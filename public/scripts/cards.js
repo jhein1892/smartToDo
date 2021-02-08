ca5b060d46c1907fb9ed1a6377e9aeca062eed38
unko-chan <kevinliang0303@gmail.com>
2021-02-08 01:23:00 -0800

    expandable and sortable cards

---
+++ public/scripts/cards.js
@@ -0,0 +1,47 @@
+Sortable.create(readlist, {
+  animation: 150,
+  group: "shared",
+});
+
+Sortable.create(watchlist, {
+  animation: 150,
+  group: "shared",
+});
+
+Sortable.create(buylist, {
+  animation: 150,
+  group: "shared",
+});
+
+Sortable.create(eatlist, {
+  animation: 150,
+  group: "shared",
+});
+
+Sortable.create(cardgrid, {
+  swapThreshold: 1,
+  animation: 150,
+});
+
+$(document).ready(function () {
+  let expanded = false;
+  $(".bi-pencil-square").click(function () {
+    if (!expanded) {
+      expanded = true;
+      $(this).closest(".card").css({
+        transform: "scale(1.5)",
+        "z-index": "10",
+        "font-size": "1em"
+      });
+      console.log(expanded);
+    } else if (expanded) {
+      expanded = false;
+      $(this).closest(".card").css({
+        transform: "",
+        "z-index": "",
+        "font-size": "",
+      });
+      console.log(expanded);
+    }
+  });
+});
