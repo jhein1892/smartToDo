192e303dab330d309457a317ffe9594ce683d16e
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-02-09 03:42:06 +0000

    Stop redirection after login

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,69 +1,76 @@
 $(() => {
   console.log("Doc READY!")
 
-  //if no cookie > display login button
-  //when click on login button > slide down input + button
-  // if user logged in > display: "logged in as: ____" + logout button
+  const login = `
+  <form id="submit-login" action="/user/login" method="POST">
+  <button type="submit" class="centerlined" for="user-login">Login</button>
+  <input type="text" id="login-form" name="user" />
+  </form>`;
+  $(".logins").prepend(login)
 
-const login = `
-<form id="submit-login" action="/user/login" method="POST">
-<button type="submit" class="centerlined" for="user-login">
-  Login
-</button>
-<input type="text" id="login-form" name="user" />
-</form>`
-$(".logins").prepend(login)
+  const logout = `<form action="/user/logout" method="POST">
+  <button type="submit" class="centerlined">Logout</button>
+</form>`;
 
 
-// const createTodo = function(todo) {
-
-// }
-
-// const renderList = function(list) {
-//   $('#list-todo').empty();
-//   list.forEach((task) => {
-//     const todo = createTodo(task);
-//     $().prepend(todo)
-//   })
-// }
-
-
-
-$('#submit-login').submit(function (event) {
-  event.preventDefault();
-
-  const user = $('#login-form');
-  if (user.val().trim() === "" || user.val().length < 0) {
-    $(".error-message").append('<div>Please enter your username</div>').slideDown()
-    return false;
+  const renderList = function(list) {
+    //MUST FIND A WAY TO REDIRECT TO DIFFERENT LOCATIONS ON DOM
+    const $list = $('#list-todo');
+    for (const id in list) {
+      const tasks = list[id];
+      for (const i in tasks) {
+        const task = tasks[i];
+        // CHANGE LOCATIONS ACCORDING TO ID
+        const $display = $('<div>').addClass('list-display')
+        const $text = $('<span>').text(task.text);
+        const $category = $('<span>').text(task.category);
+        $display.append($text, $category);
+        $list.prepend($display)
+      }
+    }
   }
 
-  $(".error-message").slideUp();
+  const loadLists = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/todo',
+      dataType: 'json',
+      success: function(tasks) {
+        console.log("data: ", tasks)
+        renderList(tasks);
+      },
+      error: (error) => {console.log(error)}
+    });
+  };
 
-  $.ajax({
-    type: "POST",
-    url: '/user/login',
-    data: {user: user.val().trim()},
-    success: function() {
-      const username = $('#login-form').val()
-      console.log('username:', username)
-      $("#submit-login").replaceWith(`<p>Welcome ${username}!</p>`);
-    },
+
+  $('#submit-login').submit(function (event) {
+    event.preventDefault();
+
+    const user = $('#login-form');
+    if (user.val().trim() === "" || user.val().length < 0) {
+      $(".error-message").append('<div>Please enter your username</div>').slideDown()
+      return false;
+    }
+
+    $(".error-message").slideUp();
+
+    $.ajax({
+      type: "POST",
+      url: '/user/login',
+      data: {user: user.val().trim()},
+      success: function() {
+        const username = $('#login-form').val()
+        console.log('logged in as:', username)
+        $("#submit-login").replaceWith(`<p>Welcome ${username}!</p>`)
+        $(".logins").append(logout);
+      },
+    })
+    .then(() => loadLists())
   });
 
-  // ISSUE: not reading properly as front-end not reading cookies passed to the back-end fnc
-  $.ajax({
-    method: 'GET',
-    url: '/todo',
-    dataType: 'json',
-    success: function(data) {
-      console.log("data: ", data)
-    },
-    error: (error) => {console.log(error)}
-  });
-});
-
-
+  // In case a page reload is made, the lists will continue to display
+  loadLists();
 
 });
 
