214e79a9e6ae0cd05c110105165b8f0879acb682
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-02-09 04:12:59 +0000

    Recover old version to working one

--- routes/users.js
+++ routes/users.js
@@ -11,35 +11,6 @@ const server = require("../server");
 
 module.exports = (db) => {
   router.get("/login", (req, res) => {
-    const templateVars = {
-      user: req.cookies["user_id"]
-    }
-    console.log("templ: ", templateVars)
-    res.render("index", templateVars)
-  });
-
-  router.post("/login", (req, res) => {
-
-    const getUser = function(username) {
-      return db.query(`SELECT id FROM users WHERE name = $1`, [username])
-      // .then(data => data.rows[0].id)
-      .then(res => console.log(res.rows))
-      .catch(err => console.log('error'))
-    }
-
-    const username = req.body.user;
-    const id = getUser(username);
-    console.log('😁:', id)
-    res.cookie("user_id", id);
-    res.redirect("/todo")
-  });
-
-  router.post("/logout", (req, res) => {
-    const templateVars = {
-      user: req.cookies["user_id"]
-    }
-    res.clearCookie("user_id");
-    res.render("index", templateVars)
     //MISSING CODE
   });
 
