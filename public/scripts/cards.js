aab70fe798235fa1ee923bf1ca2ed0bf317a4e60
unko-chan <kevinliang0303@gmail.com>
2021-02-09 13:55:53 -0800

    completed list tab with transistion

--- public/scripts/cards.js
+++ public/scripts/cards.js
@@ -28,11 +28,17 @@ const slideFade = (list) => {
   const fade = { opacity: 0, transition: "opacity 0.5s" };
   list.css(fade).slideUp();
 };
+const slideFadeDown = (list) => {
+  const fade = { opacity: 0, transition: "opacity 0.5s" };
+  list.css(fade).slideDown();
+};
 
 // Expand function
 $(document).ready(function () {
   $(".tab").hide();
+  $(".read-complete").hide();
   let expanded = false;
+  let completeTab = false;
   $(".bi-pencil-square").click(function () {
     if (!expanded) {
       expanded = true;
@@ -41,20 +47,50 @@ $(document).ready(function () {
         "z-index": "10",
         "font-size": "1em",
       });
-
       $(this).parent().siblings(".tab").slideDown(200);
     } else if (expanded) {
       expanded = false;
+      completeTab = false;
       $(this).closest(".card").css({
         transform: "",
         "z-index": "",
         "font-size": "",
       });
       $(this).parent().siblings(".tab").slideUp(200);
+      $(this)
+        .parent()
+        .siblings(".card-body")
+        .find(".read-complete")
+        .slideUp(200);
+      $(this).parent().siblings(".card-body").find(".read-todo").slideDown(200);
     }
   });
 
   $(".bi-x").click(function () {
     slideFade($(this.closest(".list-item")));
+  });
+
+  $("#completed-button").click(function () {
+    if (!completeTab) {
+      completeTab = true;
+      $(this)
+        .parent()
+        .siblings(".card-body")
+        .find(".read-complete")
+        .slideDown(200);
+      $(this).parent().siblings(".card-body").find(".read-todo").slideUp(200);
+    }
+  });
+
+  $("#todo-button").click(function () {
+    if (completeTab) {
+      completeTab = false;
+      $(this)
+        .parent()
+        .siblings(".card-body")
+        .find(".read-complete")
+        .slideUp(200);
+      $(this).parent().siblings(".card-body").find(".read-todo").slideDown(200);
+    }
   });
 });
