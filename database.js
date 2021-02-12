3d0c469f1914f97d48864ce3631762ef5cec81b6
Jacob Hein <jacobhein1892@gmail.com>
2021-02-12 00:11:31 +0000

    can add/remove item from completed list

--- database.js
+++ database.js
@@ -43,8 +43,12 @@ const completeToDo = function(todoId) {
     console.log(verdict)
     if (verdict === true){
       return db.query(`UPDATE to_dos SET completed = false WHERE id = $1`, [todoId])
-    } else {
+        .then(res => res.rows[0])
+        .catch(err => err)
+    } else if (verdict === false) {
       return db.query(`UPDATE to_dos SET completed = true WHERE id = $1`, [todoId])
+      .then(res => res.rows[0])
+      .catch(err => err)
     }
   });
 }
